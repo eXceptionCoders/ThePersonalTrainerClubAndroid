6f0811eeec56f4b36dea8c92fa50637684027340
David Lopez Rodriguez <dloprodu@gmail.com>
2018-12-11 23:00:29 +0000

    linking TrainerManagementFragment witch class detail

--- ThePersonalTrainerClub/app/src/main/java/es/exceptioncoders/thepersonaltrainerclub/view/trainerManagement/TrainerManagementFragmentNavigator.kt
+++ ThePersonalTrainerClub/app/src/main/java/es/exceptioncoders/thepersonaltrainerclub/view/trainerManagement/TrainerManagementFragmentNavigator.kt
@@ -1,6 +1,7 @@
 package es.exceptioncoders.thepersonaltrainerclub.view.trainerManagement
 
 import android.app.Activity
+import android.support.v4.app.Fragment
 import android.content.Intent
 import es.exceptioncoders.thepersonaltrainerclub.model.model.ClassModel
 import es.exceptioncoders.thepersonaltrainerclub.view.base.BaseNavigator
@@ -10,7 +11,7 @@ import es.exceptioncoders.thepersonaltrainerclub.view.trainerManagement.TrainerM
 
 class TrainerManagementFragmentNavigator : BaseNavigator<TrainerManagementFragmentContract.View>(), TrainerManagementFragmentContract.Navigator<TrainerManagementFragmentContract.View> {
     override fun navigateToClassDetail(model: ClassModel) {
-        val intent = Intent(mView as Activity, ClassDetailActivity::class.java)
-        (mView as Activity).startActivity(intent)
+        val intent = Intent((mView as Fragment).activity, ClassDetailActivity::class.java)
+        (mView as Fragment).activity!!.startActivity(intent)
     }
 }
