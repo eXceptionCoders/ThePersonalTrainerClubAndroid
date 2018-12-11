d2538c57d83e5bfa747413d0f66cbb9b33e76af6
David Lopez Rodriguez <dloprodu@gmail.com>
2018-12-11 23:35:17 +0000

    class detail implemented

--- ThePersonalTrainerClub/app/src/main/java/es/exceptioncoders/thepersonaltrainerclub/view/trainerManagement/TrainerManagementFragmentNavigator.kt
+++ ThePersonalTrainerClub/app/src/main/java/es/exceptioncoders/thepersonaltrainerclub/view/trainerManagement/TrainerManagementFragmentNavigator.kt
@@ -1,17 +1,16 @@
 package es.exceptioncoders.thepersonaltrainerclub.view.trainerManagement
 
-import android.app.Activity
 import android.support.v4.app.Fragment
 import android.content.Intent
 import es.exceptioncoders.thepersonaltrainerclub.model.model.ClassModel
 import es.exceptioncoders.thepersonaltrainerclub.view.base.BaseNavigator
 import es.exceptioncoders.thepersonaltrainerclub.view.classDetail.ClassDetailActivity
-import es.exceptioncoders.thepersonaltrainerclub.view.trainerManagement.TrainerManagementFragmentContract
 
 
 class TrainerManagementFragmentNavigator : BaseNavigator<TrainerManagementFragmentContract.View>(), TrainerManagementFragmentContract.Navigator<TrainerManagementFragmentContract.View> {
     override fun navigateToClassDetail(model: ClassModel) {
         val intent = Intent((mView as Fragment).activity, ClassDetailActivity::class.java)
+        intent.putExtra(ClassDetailActivity.MODEL_CLASS_KEY, model)
         (mView as Fragment).activity!!.startActivity(intent)
     }
 }
